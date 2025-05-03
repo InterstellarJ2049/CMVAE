5480d49ff2987a1124d35b676e17407443ee8e1b
Yijie <yijie-zhang@uiowa.edu>
2025-05-02 22:37:02 -0500

    05-2_3_gpu2_IWAE_K10_32_64_1.0_2

--- src/commands/run_CUBICC_experiment.sh
+++ src/commands/run_CUBICC_experiment.sh
@@ -8,10 +8,10 @@ SEED=2
 SHARED_LAT_DIM=64
 MS_LAT_DIM=32
 
-gpuid=1
+gpuid=2
 
 # Train CMVAE
-CUDA_VISIBLE_DEVICES=${gpuid} python train_CMVAE_CUBICC.py --experiment $EXPERIMENT --obj "iwae" --K 1 --batch-size 32 --epochs $EPOCHS \
+CUDA_VISIBLE_DEVICES=${gpuid} python train_CMVAE_CUBICC.py --experiment $EXPERIMENT --obj "iwae" --K 10 --batch-size 32 --epochs $EPOCHS \
       --latent-dim-c 35 --latent-dim-z $SHARED_LAT_DIM --latent-dim-w $MS_LAT_DIM --seed $SEED --beta 1.0 \
       --datadir $DATADIR  --outputdir $OUTPUTDIR \
       --inception_path "${DATADIR}/pt_inception-2015-12-05-6726825d.pth" \
